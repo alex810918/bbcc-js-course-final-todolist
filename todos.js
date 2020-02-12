6c5fb0f0651a00d06221a866841984eba5bf3d9a
dannyhuang <kuanhsuh@gmail.com>
2020-02-12 14:42:02 +0800

    Updated Final

--- todos.js
+++ todos.js
@@ -1,87 +1,93 @@
 class Todos {
-  constructor() {
-    this.todos = [
-      {
-        id: 0,
-        todoText: "Buy Milk",
-        completed: false
-      },
-      {
-        id: 1,
-        todoText: "Do Home Work",
-        completed: false
-      },
-      {
-        id: 2,
-        todoText: "Pick Up Kids",
-        completed: true
-      },
-      {
-        id: 3,
-        todoText: "Family Trip",
-        completed: true
-      },
-      {
-        id: 4,
-        todoText: "Conference Meeting",
-        completed: true
-      },
-      {
-        id: 5,
-        todoText: "Meeting at 5",
-        completed: true
-      }
-    ]
-    this.todoList = document.querySelector("#todoList")
-    this.addTodoBtn = document.querySelector("#addTodoBtn")
-    this.newTodoModal = document.querySelector("#newTodoModal")
-    this.updateTodoModal = document.querySelector("#updateTodoModal")
-    this.addNewModalCross = document.querySelector("#addNewModalCross")
-    this.updateModalCross = document.querySelector("#updateModalCross")
-    this.addTodoTextInput = document.querySelector("#addTodoTextInput")
-    this.updateTodoTextInput = document.querySelector("#updateTodoTextInput")
-    this.addNewBtn = document.querySelector("#addNewBtn")
-    this.updateModalBtn = document.querySelector("#updateModalBtn")
-    this.toggleAllButton = document.querySelector("#toggleAllButton")
-    this.events()
-  }
+    // 所有DOM 元件/ Todo變數
+    constructor() {
+        this.todos = [
+            {
+                id: 0,
+                todoText: 'Buy Milk',
+                completed: false,
+            },
+            {
+                id: 1,
+                todoText: 'Do Home Work',
+                completed: false,
+            },
+            {
+                id: 2,
+                todoText: 'Pick Up Kids',
+                completed: true,
+            },
+            {
+                id: 3,
+                todoText: 'Family Trip',
+                completed: true,
+            },
+            {
+                id: 4,
+                todoText: 'Conference Meeting',
+                completed: true,
+            },
+            {
+                id: 5,
+                todoText: 'Meeting at 5',
+                completed: true,
+            },
+        ]
+        this.todoList = document.querySelector('#todoList')
+        this.addTodoBtn = document.querySelector('#addTodoBtn')
+        this.newTodoModal = document.querySelector('#newTodoModal')
+        this.updateTodoModal = document.querySelector('#updateTodoModal')
+        this.addNewModalCross = document.querySelector('#addNewModalCross')
+        this.updateModalCross = document.querySelector('#updateModalCross')
+        this.addTodoTextInput = document.querySelector('#addTodoTextInput')
+        this.updateTodoTextInput = document.querySelector(
+            '#updateTodoTextInput'
+        )
+        this.addNewBtn = document.querySelector('#addNewBtn')
+        this.updateModalBtn = document.querySelector('#updateModalBtn')
+        this.toggleAllButton = document.querySelector('#toggleAllButton')
+        this.events()
+    }
 
-  events() {
-    this.renderTodos()
-    this.addTodoBtn.addEventListener("click", e => this.toggleAddNewModal(e))
-    this.addNewModalCross.addEventListener("click", e =>
-      this.toggleAddNewModal(e)
-    )
-    this.updateModalCross.addEventListener("click", e =>
-      this.toggleUpdateModal(e)
-    )
-    this.addNewBtn.addEventListener("click", e => this.addTodo(e))
-    this.toggleAllButton.addEventListener("click", e => this.toggleAll())
-    this.updateModalBtn.addEventListener("click", e => this.updatetodo(e))
-    // Event delegation
-    this.todoList.addEventListener("click", e => {
-      if (e.target.id === "toggleComplete") {
-        this.toggleTodo(e.target.dataset.item)
-      }
-      if (e.target.id === "deleteTodoBtn") {
-        this.deleteTodo(e.target.dataset.item)
-      }
-      if (e.target.id === "updateTodoBtn") {
-        this.updateTodoTextInput.value = this.todos[
-          e.target.dataset.item
-        ].todoText
-        this.updateTodoModal.dataset.item = e.target.dataset.item
-        this.toggleUpdateModal()
-      }
-    })
-  }
+    // 所有事件處理器
+    events() {
+        this.renderTodos()
+        this.addTodoBtn.addEventListener('click', e =>
+            this.toggleAddNewModal(e)
+        )
+        this.addNewModalCross.addEventListener('click', e =>
+            this.toggleAddNewModal(e)
+        )
+        this.updateModalCross.addEventListener('click', e =>
+            this.toggleUpdateModal(e)
+        )
+        this.addNewBtn.addEventListener('click', e => this.addTodo(e))
+        this.toggleAllButton.addEventListener('click', e => this.toggleAll())
+        this.updateModalBtn.addEventListener('click', e => this.updatetodo(e))
+        // Event delegation
+        this.todoList.addEventListener('click', e => {
+            if (e.target.id === 'toggleComplete') {
+                this.toggleTodo(e.target.dataset.item)
+            }
+            if (e.target.id === 'deleteTodoBtn') {
+                this.deleteTodo(e.target.dataset.item)
+            }
+            if (e.target.id === 'updateTodoBtn') {
+                this.updateTodoTextInput.value = this.todos[
+                    e.target.dataset.item
+                ].todoText
+                this.updateTodoModal.dataset.item = e.target.dataset.item
+                this.toggleUpdateModal()
+            }
+        })
+    }
 
-  // Render todos
-  renderTodos() {
-    let viewTodos = this.todos
-      .map(todo => {
-        if (todo.completed) {
-          return `
+    // Render todos
+    renderTodos() {
+        let viewTodos = this.todos
+            .map(todo => {
+                if (todo.completed) {
+                    return `
             <li class="border-b border-purple-400 text-xl py-4 flex justify-between items-center">
         <div class="flex items-center">
           <img id="toggleComplete" data-item="${todo.id}" class="w-8 mr-2"
@@ -94,8 +100,8 @@ class Todos {
         </div>
       </li>
             `
-        } else {
-          return `
+                } else {
+                    return `
             <li class="border-b border-purple-400 text-xl py-4 flex justify-between items-center">
               <div class="flex items-center">
                 <span id="toggleComplete" data-item="${todo.id}" class="inline-block mr-2 w-8 h-8 bg-purple-200"></span>${todo.todoText}
@@ -106,83 +112,83 @@ class Todos {
               </div>
             </li>
             `
-        }
-      })
-      .join(" ")
-    this.todoList.innerHTML = viewTodos
-  }
-
-  // Delete Todos
-  deleteTodo(id) {
-    console.log(id)
-    this.todos.splice(id, 1)
-    this.renderTodos()
-  }
-
-  // Toggle All
-  toggleAll() {
-    let todoTodos = this.todos.length
-    let completedTodo = 0
-
-    this.todos.forEach(function(todo, index) {
-      if (todo.completed === true) {
-        completedTodo++
-      }
-    })
-    if (completedTodo === todoTodos) {
-      for (var i = 0; i < todoTodos; i++) {
-        this.todos[i].completed = false
-      }
-    } else {
-      for (var i = 0; i < todoTodos; i++) {
-        this.todos[i].completed = true
-      }
+                }
+            })
+            .join(' ')
+        this.todoList.innerHTML = viewTodos
     }
-    this.renderTodos()
-  }
 
-  // Toggle Todo
-  toggleTodo(id) {
-    this.todos[id].completed = !this.todos[id].completed
-    this.renderTodos()
-  }
+    // Delete Todos
+    deleteTodo(id) {
+        console.log(id)
+        this.todos.splice(id, 1)
+        this.renderTodos()
+    }
 
-  // Add Todo
-  addTodo(e) {
-    e.preventDefault()
-    this.todos.push({
-      id: this.todos.length,
-      todoText: this.addTodoTextInput.value,
-      completed: false
-    })
-    this.addTodoTextInput.value = ""
-    this.toggleAddNewModal()
-    this.renderTodos()
-  }
+    // Toggle All
+    toggleAll() {
+        let todoTodos = this.todos.length
+        let completedTodo = 0
 
-  updatetodo(e) {
-    e.preventDefault()
-    this.todos[
-      this.updateTodoModal.dataset.item
-    ].todoText = this.updateTodoTextInput.value
-    this.updateTodoModal.dataset.item = ""
-    this.toggleUpdateModal()
-    this.renderTodos()
-  }
+        this.todos.forEach(function(todo, index) {
+            if (todo.completed === true) {
+                completedTodo++
+            }
+        })
+        if (completedTodo === todoTodos) {
+            for (var i = 0; i < todoTodos; i++) {
+                this.todos[i].completed = false
+            }
+        } else {
+            for (var i = 0; i < todoTodos; i++) {
+                this.todos[i].completed = true
+            }
+        }
+        this.renderTodos()
+    }
 
-  // Toggle Add New Modal
-  // 先設定 e 是空值
-  toggleAddNewModal(e = "") {
-    // 如果參數有e就preventDefault, 不然就繼續執行
-    e && e.preventDefault()
-    this.newTodoModal.classList.toggle("hidden")
-  }
+    // Toggle Todo
+    toggleTodo(id) {
+        this.todos[id].completed = !this.todos[id].completed
+        this.renderTodos()
+    }
 
-  // Toggle Update Todo Modal
-  toggleUpdateModal(e = "") {
-    e && e.preventDefault()
-    this.updateTodoModal.classList.toggle("hidden")
-  }
+    // Add Todo
+    addTodo(e) {
+        e.preventDefault()
+        this.todos.push({
+            id: this.todos.length,
+            todoText: this.addTodoTextInput.value,
+            completed: false,
+        })
+        this.addTodoTextInput.value = ''
+        this.toggleAddNewModal()
+        this.renderTodos()
+    }
+
+    updatetodo(e) {
+        e.preventDefault()
+        this.todos[
+            this.updateTodoModal.dataset.item
+        ].todoText = this.updateTodoTextInput.value
+        this.updateTodoModal.dataset.item = ''
+        this.toggleUpdateModal()
+        this.renderTodos()
+    }
+
+    // Toggle Add New Modal
+    // 先設定 e 是空值
+    toggleAddNewModal(e = '') {
+        // 如果參數有e就preventDefault, 不然就繼續執行
+        e && e.preventDefault()
+        this.newTodoModal.classList.toggle('hidden')
+    }
+
+    // Toggle Update Todo Modal
+    toggleUpdateModal(e = '') {
+        e && e.preventDefault()
+        this.updateTodoModal.classList.toggle('hidden')
+    }
 }
 
 var todo = new Todos()
